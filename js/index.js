1cc1c7f11bb89bad26e8796c1d3862570483144e
Sarah L. Katz <soshasarah@gmail.com>
2017-02-25 21:42:16 -0500

    Created Background-Changing JS
    Added in javascript/jQuery to change the background when the user inputs
    a city

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
 $(function() {
 
   // 1. Retrieve user input (city)
+
+  $("#submit-btn").click(change);
+
   // 2. Store user input as a variable
+
+  function change(event) {
+    event.preventDefault();
+    var inputCity = $("#city-type").val();
+    var city = inputCity.toLowerCase();
+
   // 3. Change background image by adding a class
+
+    if (city === "new york city" || city === "new york" || city === "nyc") {
+      $("body").addClass("nyc");
+    }
+
+    else if (city === "san francisco" || city === "sf" || city === "bay area") {
+      $("body").addClass("sf");
+    }
+
+    else if (city === "los angeles" || city === "la" || city === "lax") {
+      $("body").addClass("la");
+    }
+
+    else if (city === "austin" || city === "atx") {
+      $("body").addClass("austin");
+    }
+
+    else if (city === "sydney" || city === "syd") {
+      $("body").addClass("sydney");
+    }
+
+    else {
+      alert("Please Choose a Different City");
+    }
+
+  };
+
 });
