29ca80cdea5a6eb00da70f1ee2662632bd1e2873
Tavernese, Mikaela <Mikaela.Tavernese@aenetworks.com>
2017-02-27 19:43:54 -0500

    changes on 1 word, error on none, red on no match

--- js/main.js
+++ js/main.js
@@ -9,11 +9,33 @@ $("form").submit(enter);
 //3. fetch and store city in variable 
 function enter(e) {
   e.preventDefault();
-  var newCity = $("#city-type").val();
-  console.log(newCity);
+  var city = $("#city-type").val();
+  console.log(city);
+  console.log(typeof city === "string")
+
+//4. error on empty field 
+  if (city === "") {
+     alert('please enter a city')
+   } else {
+
+  //5. Display background image matching variable 
+if (city === "nyc") {
+  $('body').css('background', 'url(images/nyc.jpg)');
+} 
+else if (city === "San Francisco") {
+  $('body').css('background', 'url(images/sf.jpg)');
 }
 
-//4. Display background image matching variable 
+//6. display defualt background if nothing matches
+else {
+  $('body').css('background', 'red');
+}
+
+
+}
+}
+
+
 //5. clear out text field
 
 });
