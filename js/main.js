bc9cfbd6ff81ba28d7d835a46e2cb3b89ded2ac3
rfrieds <rachels.friedman@gmail.com>
2017-02-27 20:10:11 -0500

    Fixed Image Path
    Absolute path used in jquery not relative path

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// ###Technical Requirements
-
-// - Write at least six different lines of pseudocode and display them inline as JavaScript comments
-// - Get the first element from an attribute name using ```$.attr()```
-
 $ (function() {
 
   // 1. Define variable
@@ -15,29 +10,37 @@ $ (function() {
 
   // 3. Declare a function to retrieve values from field and store in variable
 
+  // Declare function
   function scan(event) {
+  
+    // Clear default event functionality 
     event.preventDefault();
-    var city = $('#city-type').val();
     
-    // print in console log to confirm function working and variable type 
+    // Store variable 
+    var city = $("#city-type").val();
+    
+    // Print in console log to confirm variable stored and variable type 
     console.log(city);
     console.log(typeof city === 'string');
 
   // 4. Change backgroud Image Based on Entries
 
     if (city === 'New York' || city === 'NYC' || city === 'New York City') {
-        $('body').css('background-image','url(../images/nyc.jpg)');
+        $('body').css('background-image','url(images/nyc.jpg)');
       } else if (city === 'San Francisco' || city === 'Bay Area' || city === 'SF') {
-        $('body').css('background-image','url(../images/sf.jpg)');
+        $('body').css('background-image','url(images/sf.jpg)');
       } else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX') {
-        $('body').css('background-image','url("../images/la.jpg")');
+        $('body').css('background-image','url(images/la.jpg)');
       } else if (city === 'Austin' || city === 'ATX') {
-        $('body').css('background-image','url("../images/austin.jpg")');
+        $('body').css('background-image','url(images/austin.jpg)');
       } else if (city === 'Sydney' || city === 'SYD') {
-        $('body').css('background-image','url("../images/sydney.jpg")');
-      } else if(city === 'test')  {
-        alert('syntax is working');
-      } 
-      // Remove test when complete
+        $('body').css('background-image','url(images/sydney.jpg)');
+      // *This is test code used to confirm functionality. Do not use* 
+      // } else if(city === 'test')  {
+      //   alert('syntax is working');
+      } else {
+        $('body').css('background-image','url(images/citipix_skyline.jpg)');
+      }
+
   }
 });
