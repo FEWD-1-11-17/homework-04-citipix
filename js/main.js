738df3abb8b1491b468c99b243917e06b3fc5371
ChiJohn80 <chiaka.john@gmail.com>
2017-02-28 23:49:46 -0500

    added if else

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 $(function() {
-  var city = 0;
+  var city = ['nyc', 'sf', 'sydney','la', 'austin'];
 
-$("#entry").submit(scan);
+  $('#submitButton').click(changeBackground);
+  
+  function changeBackground() {
+    event.preventDefault()
 
-function scan(event){
-  event.preventDefault();
+    var city = $(this).attr("id");
+    if (city === 'nyc' || 'new york city' || 'new york') {
+      $('backgroundImage').css("backgroundImage", "url(../images/nyc.jpg))");
+    
+    } else if (city === 'sf' || 'san francisco' || 'bay area') {
+      $('backgroundImage').css("backgroundImage", "url(../images/sf.jpg))");
 
-  var string = $("#newentry").val();
+    } else if (city === 'sydney' || 'syd') {
+      $('backgroundImage').css("backgroundImage", "url(../images/sydney.jpg))");
+    
+    } else if (city === 'los angeles' || 'la' || 'lax') {
+      $('backgroundImage').css("backgroundImage", "url(../images/la.jpg))");
 
-  $("#newentry").val("");
-}
+    } else if (city === 'austin' || 'atx') {
+      $('backgroundImage').css("backgroundImage", "url(../images/austin.jpg))");
+
+    } else{
+        alert("Try a Homework City");
+    }
+  }
+
 });
