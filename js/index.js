f9d4f2c9afc7575796e5bf25a9ca103e7c1ab40e
Nico <nicouly@gmail.com>
2017-03-23 22:40:07 -0400

    revisions

--- js/index.js
+++ js/index.js
@@ -8,23 +8,23 @@ $(function() {
       // console.log(inputinfo);
       if (inputinfo === "New York" || inputinfo === "NYC" || inputinfo === "New York City" || inputinfo === "nyc") {
       console.log('empire');
-      $('body').css('background-image', 'url(../homework-04-citipix/images/nyc.jpg)')
+      $('body').css('background-image', 'url(../images/nyc.jpg)')
 
     } if (inputinfo === "la" || inputinfo === "LA" || inputinfo === "LAX" || inputinfo === "Los Angeles") {
       console.log('famous');
-      $('body').css('background-image', 'url(../homework-04-citipix/images/la.jpg)')
+      $('body').css('background-image', 'url(../images/la.jpg)')
 
     } if (inputinfo === "Austin" || inputinfo === "ATX") {
       console.log('texas');
-      $('body').css('background-image', 'url(../homework-04-citipix/images/austin.jpg)')
+      $('body').css('background-image', 'url(../images/austin.jpg)')
 
     } if (inputinfo === "San Fransico" || inputinfo === "SF" || inputinfo === "Bay Area" || inputinfo === "sf") {
       console.log('hyphy');
-      $('body').css('background-image', 'url(../homework-04-citipix/images/sf.jpg)')
+      $('body').css('background-image', 'url(../images/sf.jpg)')
 
     } if (inputinfo === "Sydney" || inputinfo === "SYD"){
       console.log('manly beach');
-      $('body').css('background-image', 'url(../homework-04-citipix/images/sydney.jpg)')
+      $('body').css('background-image', 'url(../images/sydney.jpg)')
     }
   }
 
